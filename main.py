c02920e1d54b066dd670aa2cb81b60a024eeb687
Xyl <88043949+X-y-l@users.noreply.github.com>
2022-05-02 14:18:19 +0100

    Update main.py
    added the find_path_across function

--- main.py
+++ main.py
@@ -3,6 +3,78 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+def find_path_across(grid):
+    '''
+    For a numpy array of booleans (the yellow/blue grid), this function returns True or False
+    depending on whether or not a yellow path exists from the left to the right edge.
+    It will also display the path if you pass True
+    This is not efficient
+    '''
+
+    # we know all the yellows on the left are reachable
+    grid_size = np.shape(grid)
+    x_length = grid_size[1]
+    y_length = grid_size[0]
+    # we make an array of the same size as the grid to store
+    # which squares we can reach. Initially we assume we can't reach any
+    # 0 represents a square is unreachable
+    reachable = np.full(grid_size, 0)
+    # yellows is a list of arrays which contain the coordinates of each yellow square
+    yellows = np.argwhere(grid == True)
+
+    # we store the x and y coordinates of the points in yellows in seperate arrays in a larger array yellows_xy
+    yellows_xy = np.array(yellows).T
+
+    # we find the indices in the x coordinate array of each yellow in the left hand column of the grid
+    indices = np.where(yellows_xy[1] == 0)
+
+    # using the indices we found we now find the y coordinate of these yellow squares in the first column
+    # we iterate over each index to assign 1 to reachable[y,0] where [y,0] is yellow.
+    # 1 signifies a yellow reachable square in reachable while all currently unreached squares are 0
+    for i in (indices[0]):
+        reachable[yellows_xy[0][i], 0] = 1
+
+    # we create an array of all coordinates of sites already checked in the form of a list of lists
+    reachable_array = np.asarray(np.where(reachable == 1)).T.tolist()
+
+    # now we search for adjacent yellow squares to the ones we already have
+    # we iterate over our array of coordinates of reachable squares
+    # we assign y and x for each squares so that we can later compare to adjacent sites
+    for square in reachable_array:
+        y = square[0]
+        x = square[1]
+        # As soon as a site on the RHS is shown to be reachable we return True
+        if 1 in reachable[:, x_length - 1]:
+            return True
+        # check adjacent
+        # the first ifs in each part stops us getting index out of bounds errors
+        # we check !=1 because otherwise we'd find the same squares again
+        # and it would loop forever
+        else:
+            if x < x_length - 1:
+                # checks right
+                if grid[y, x + 1] == True and reachable[y, x + 1] != 1:
+                    reachable[y, x + 1] = 1
+                    reachable_array.append([y, x + 1])
+            if y - 1 >= 0:
+                # checks below
+                if grid[y - 1, x] == True and reachable[y - 1, x] != 1:
+                    reachable[y - 1, x] = 1
+                    reachable_array.append([y - 1, x])
+            if y < y_length - 1:
+                # checks above
+                if grid[y + 1, x] == True and reachable[y + 1, x] != 1:
+                    reachable[y + 1, x] = 1
+                    reachable_array.append([y + 1, x])
+            if x - 1 >= 0:
+                # checks left
+                if grid[y, x - 1] == True and reachable[y, x - 1] != 1:
+                    reachable[y, x - 1] = 1
+                    reachable_array.append([y, x - 1])
+
+    # Return False if after no further reachable path is found and we have not reached the right hand side of the grid
+    return False
+
 
 def display_rand_grid(grid_size, p_yellow):
     """
